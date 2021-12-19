c7e62ad5555c5accefc1e5fb39d601f6d95bdc7c
KanapkaBezMasla <58138205+KanapkaBezMasla@users.noreply.github.com>
2021-12-19 17:03:26 +0100

    Zamiana 0,67 na stałą CONVERTER

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -1,8 +1,9 @@
 from PIL import Image
-import cv2
-from PyQt5.QtWidgets import QDesktopWidget
+from PyQt5.QtWidgets import QDesktopWidget, QWidget
 from openpyxl import Workbook, load_workbook
 import os
+
+from Preprocessing import Preprocessing
 from WarningWindow import WarningWindow
 import math
 
@@ -10,6 +11,8 @@ import math
 
 class ImageProcessing:
 
+    def __init__(self):
+        self.preProc = Preprocessing()
 
     def findFirstChanOnImg(chanY: int, chanN: int, pixOfMark: int, yBeg: int, yDest: int, compYPix: int):
         ymin = 155 + (compYPix - 1) * 3
@@ -27,32 +30,47 @@ class ImageProcessing:
         return chanN, pixOfMark
 
 
-    @staticmethod
-    def binarization(openFile: str, savingFile: str, threshold: int):
-        img = cv2.imread(openFile)
-        th, im_th = cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY)
-        cv2.imwrite(savingFile, im_th)
-
-    @staticmethod
-    def measurement(mmPerPix: int, chanY: int, markedChan: int, pixLine: int, xBeg: int, yBeg: int, yDest: int, compYPix: int, x_scale_val: int, x_scale_pos: int):
+    def measurement(self, xBeg: int, yBeg: int, yDest: int, app: QWidget):
         """Funkcja służąca pomierzeniu uszkodzeń i zapisaniu ich do pliku"""
 
-        #ustalenie x dla początku screena względem osi
+        mmPerPix = self.preProc.readNumber(43, 96, 120, 120, app, 'x')
+        compYPix = self.preProc.readNumber(355, 96, 397, 120, app, 'compY Pixels')
+        x_scale_val, x_scale_pos = self.preProc.findBeltX()
+        markedChan, pixLine, chanY = self.preProc.findBeltChan()
+
+        self.preProc.binarization('markedArea.png', 'binarizated.png', 200)
+
+        #ustalenie x dla poczatku screena wzgledem osi
+        CONVERTER = 0.67    #zmienna przeliczająca mmPerPix na liczbę milimetrów odpowiadających 1 pikselowi
         x_scale_val *= 1000
-        x_scale_val += round(((xBeg - x_scale_pos)*mmPerPix)*0.67)
+        x_scale_val += round(((xBeg - x_scale_pos)*mmPerPix)*CONVERTER)
         chanN, pixLine = ImageProcessing.findFirstChanOnImg(chanY, markedChan, pixLine, yBeg, yDest, compYPix)
         if pixLine < 0:
-            WarningWindow('Błąd odczytu osi Y! Pomiar nie wykonany!')
+            WarningWindow('Blad odczytu osi Y! Pomiar nie wykonany!')
             return
         pixLine = pixLine - yBeg
 
         # otwieranie pliku
         if os.path.isfile('pomiaryUszkodzen.xlsx') and os.access('pomiaryUszkodzen.xlsx', os.W_OK):
             wb = load_workbook('pomiaryUszkodzen.xlsx')
+            ws = wb.active
+            ws.title = "dane"
         else:
             wb = Workbook()
-        ws = wb.active
-        ws.title = "dane"
+            ws = wb.active
+            ws.title = "dane"
+            headingRow = []
+            headingRow.append("Kanal")
+            headingRow.append("Poczatek [m]")
+            headingRow.append("Kolor")
+            headingRow.append("Uszkodzenie [mm]")
+            headingRow.append("Poczatek [m]")
+            headingRow.append("Kolor")
+            headingRow.append("Uszkodzenie [mm]")
+            headingRow.append("Poczatek [m]")
+            headingRow.append("Kolor")
+            headingRow.append("Uszkodzenie [mm]")
+            ws.append(headingRow)
         binarizated = Image.open('binarizated.png')
         width, height = binarizated.size
         yellow = True           # opisuje, czy aktualnie mierzymy obszar niebieski, czy zolty
@@ -67,27 +85,32 @@ class ImageProcessing:
         ymax = QDesktopWidget().screenGeometry().height()*2 - 90
         ymax = min(yDest, ymax) # ostatni wiersz pikseli na ktorym nalezy wykonac pomiary
         while pixLine + yBeg < ymax:
-            # Jeżeli poprzedni wiersz kończył się w pasku, a nie w "szarej strefie"
+            # Jezeli poprzedni wiersz konczyl sie w pasku, a nie w "szarej strefie"
             if countingOn:
                 if yellow == True:
-                    sheetRow.append('pocz: ' + str(float(x_scale_val + round(float((x - damageLen - greenCounting) * mmPerPix) * 0.67))/1000) + 'm')
-                    sheetRow.append('z ' + str(round(float((damageLen + greenCounting) * mmPerPix) * 0.67)) + 'mm')
-                    #ws.append()
+                    # Wpisanie pozycji poczatku uszkodzenia
+                    sheetRow.append(str(float(x_scale_val + round(float((x - damageLen - greenCounting) * mmPerPix) * CONVERTER))/1000))
+                    # Wpisanie koloru uszkodzenia
+                    sheetRow.append('zolty')
+                    sheetRow.append('z ' + str(round(float((damageLen + greenCounting) * mmPerPix) * CONVERTER)))
                 else:
-                    sheetRow.append('pocz: ' + str(float(x_scale_val + round(float((x - damageLen - greenCounting) * mmPerPix) * 0.67))/1000) + 'm')
-                    sheetRow.append('n ' + str(round(float((damageLen + greenCounting) * mmPerPix) * 0.67)) + 'mm')
+                    # Wpisanie pozycji poczatku uszkodzenia
+                    sheetRow.append(str(float(x_scale_val + round(float((x - damageLen - greenCounting) * mmPerPix) * CONVERTER))/1000))
+                    # Wpisanie koloru uszkodzenia
+                    sheetRow.append('niebieski')
+                    sheetRow.append('n ' + str(round(float((damageLen + greenCounting) * mmPerPix) * CONVERTER)))
                 damageLen = 0
                 countingOn = False
                 greenCounting = 0
                 redCounting = 0
                 if not showedWarning:
-                    WarningWindow('Możliwe ucięcie paska! Możliwie źle zapisane dane!')
+                    WarningWindow('Mozliwe uciecie paska! Mozliwie zle zapisane dane!')
                     showedWarning = True
             if sheetRow != []:
                 ws.append(sheetRow)
                 emptyChan = False
             else:
-                # Jeżeli poprzedni kanał nie był pusty to zrobi pusty wiersz w Excelu
+                # Jezeli poprzedni kanal nie byl pusty to zrobi pusty wiersz w Excelu
                 if emptyChan == False:
                     ws.append(sheetRow)
                 emptyChan = True
@@ -96,63 +119,76 @@ class ImageProcessing:
             for x in range(width):
                 p = binarizated.getpixel((x, pixLine))
                 if p[1] == 255:
-                    # Jeśli kolor żółty
+                    # Jesli kolor zolty
                     if p[0] == 255 and p[2] == 0:
                         if x == 0 and not showedWarning:
-                            WarningWindow('Możliwe ucięcie paska! Możliwie źle zapisane dane!')
+                            WarningWindow('Mozliwe uciecie paska! Mozliwie zle zapisane dane!')
                             showedWarning = True
                         if firstDamageOnChan:
                             firstDamageOnChan = False
-                            sheetRow.append('Chan' + str(chanN))
-                        # Przed żółtym jest czerwony
+                            # Numer kanalu
+                            sheetRow.append(str(chanN))
+                        # Przed zoltym jest czerwony
                         if redCounting > 0:
                             if yellow:
                                 damageLen += redCounting + 1
                             else:
                                 yellow = True
                             redCounting = 0
-                        # Przed żółtym jest też piksel żółty
+                        # Przed zoltym jest tez piksel zolty
                         elif countingOn == True and yellow == True and greenCounting == 0:
                             damageLen += 1
-                        # Przed żółtym jest niebieski
+                        # Przed zoltym jest niebieski
                         elif countingOn == True and yellow == False and greenCounting == 0:
-                            sheetRow.append('pocz: ' + str(float(x_scale_val + round(float((x - damageLen - greenCounting) * mmPerPix) * 0.67))/1000) + 'm')
-                            sheetRow.append('n ' + str(round(float((damageLen + greenCounting) * mmPerPix) * 0.67)) + 'mm')
+                            # Wpisanie pozycji poczatku uszkodzenia
+                            sheetRow.append(str(float(x_scale_val + round(float((x - damageLen - greenCounting) * mmPerPix) * CONVERTER))/1000))
+                            # Wpisanie koloru uszkodzenia
+                            sheetRow.append('niebieski')
+                            # Wpisanie dlugosci uszkodzenia
+                            sheetRow.append(str(round(float((damageLen + greenCounting) * mmPerPix) * CONVERTER)))
                             damageLen = 1
                             yellow = True
-                        # Przed żółtym jest szary
+                        # Przed zoltym jest szary
                         elif countingOn == False:
                             countingOn = True
                             damageLen = 1
                             yellow = True
-                        # Przed żółtym jest zielony
+                        # Przed zoltym jest zielony
                         else:
-                            sheetRow.append('pocz: ' + str(float(x_scale_val + round(float((x - damageLen - greenCounting) * mmPerPix) * 0.67))/1000) + 'm')
-                            sheetRow.append('n ' + str(round(float((damageLen + greenCounting) * mmPerPix) * 0.67)) + 'mm')
+                            # Wpisanie pozycji poczatku uszkodzenia
+                            sheetRow.append(str(float(x_scale_val + round(float((x - damageLen - greenCounting) * mmPerPix) * CONVERTER))/1000))
+                            # Wpisanie koloru uszkodzenia
+                            sheetRow.append('niebieski')
+                            # Wpisanie dlugosci uszkodzenia
+                            sheetRow.append(str(round(float((damageLen + greenCounting) * mmPerPix) * CONVERTER)))
                             damageLen = greenCounting + 1
                             greenCounting = 0
                             yellow = True
-                    # Jeśli kolor niebieski
+                    # Jesli kolor niebieski
                     elif p[0] == 0 and p[2] == 255:
                         if x == 0 and not showedWarning:
-                            WarningWindow('Możliwe ucięcie paska! Możliwie źle zapisane dane!')
+                            WarningWindow('Mozliwe uciecie paska! Mozliwie zle zapisane dane!')
                             showedWarning = True
                         if firstDamageOnChan:
                             firstDamageOnChan = False
-                            sheetRow.append('Chan' + str(chanN))
+                            sheetRow.append(str(chanN))
                         if redCounting > 0:
                             if yellow == False:
                                 damageLen += redCounting + 1
                             else:
                                 yellow = False
                             redCounting = 0
-                        # Przed niebieskim jest też piksel niebieski
+                        # Przed niebieskim jest tez piksel niebieski
                         elif countingOn == True and yellow == False and greenCounting == 0:
                             damageLen += 1
-                        # Przed niebieskim jest żółty
+                        # Przed niebieskim jest zolty
                         elif countingOn == True and yellow == True and greenCounting == 0:
-                            sheetRow.append('pocz: ' + str(float(x_scale_val + round(float((x - damageLen - greenCounting) * mmPerPix) * 0.67))/1000) + 'm')
-                            sheetRow.append('z ' + str(round(float((damageLen + greenCounting) * mmPerPix) * 0.67)) + 'mm')
+                            # Wpisanie pozycji poczatku uszkodzenia
+                            sheetRow.append(str(float(x_scale_val + round(float((x - damageLen - greenCounting) * mmPerPix) * CONVERTER))/1000))
+                            # Wpisanie koloru uszkodzenia
+                            sheetRow.append('zolty')
+                            # Wpisanie dlugosci uszkodzenia
+                            sheetRow.append(str(round(float((damageLen + greenCounting) * mmPerPix) * CONVERTER)))
                             damageLen = 1
                             yellow = False
                         # Przed niebieskim jest szary
@@ -163,8 +199,12 @@ class ImageProcessing:
                         # Przed niebieskim jest zielony
                         else:
                             yellow = False
-                            sheetRow.append('pocz: ' + str(float(x_scale_val + round(float((x - damageLen - greenCounting) * mmPerPix) * 0.67))/1000) + 'm')
-                            sheetRow.append('z ' + str(round(float((damageLen + greenCounting) * mmPerPix) * 0.67)) + 'mm')
+                            # Wpisanie pozycji poczatku uszkodzenia
+                            sheetRow.append(str(float(x_scale_val + round(float((x - damageLen - greenCounting) * mmPerPix) * CONVERTER))/1000))
+                            # Wpisanie koloru uszkodzenia
+                            sheetRow.append('zolty')
+                            # Wpisanie dlugosci uszkodzenia
+                            sheetRow.append(str(round(float((damageLen + greenCounting) * mmPerPix) * CONVERTER)))
                             damageLen = greenCounting + 1
                             greenCounting = 0
                     # kolor zielony
@@ -172,32 +212,40 @@ class ImageProcessing:
                         greenCounting += 1
 
 
-                # kolor szary (lub jakiś błąd koloru czerwonego/niebieskiego)
+                # kolor szary (lub jakis blad koloru czerwonego/niebieskiego)
                 else:
                     # kolor czerwony
                     if p[0] == 255:
                         if not showedWarning2:
                             WarningWindow(
-                                'Usun czerwony wskaznik z zaznaczonego pola! Dane nie zostały zapisane!')
+                                'Usun czerwony wskaznik z zaznaczonego pola! Dane nie zostaly zapisane!')
                             showedWarning2 = True
                             break
                         if countingOn:
                             redCounting += 1
                     else:
                         redCounting = 0
-                    # zakończenie paska...
+                    # zakonczenie paska...
                     if countingOn:
                         countingOn = False
-                        # ...żółtego
+                        # ...zoltego
                         if yellow == True:
-                            sheetRow.append('pocz: ' + str(float(x_scale_val + round(float((x - damageLen - greenCounting) * mmPerPix) * 0.67))/1000) + 'm')
-                            sheetRow.append('z ' + str(round(float((damageLen + greenCounting) * mmPerPix) * 0.67)) + 'mm')
+                            # Wpisanie pozycji poczatku uszkodzenia
+                            sheetRow.append(str(float(x_scale_val + round(float((x - damageLen - greenCounting) * mmPerPix) * CONVERTER))/1000))
+                            # Wpisanie koloru uszkodzenia
+                            sheetRow.append('zolty')
+                            # Wpisanie dlugosci uszkodzenia
+                            sheetRow.append(str(round(float((damageLen + greenCounting) * mmPerPix) * CONVERTER)))
                             damageLen = 0
                             greenCounting = 0
                         # ...niebieskiego
                         else:
-                            sheetRow.append('pocz: ' + str(float(x_scale_val + round(float((x - damageLen - greenCounting) * mmPerPix) * 0.67))/1000) + 'm')
-                            sheetRow.append('n ' + str(round(float((damageLen + greenCounting) * mmPerPix) * 0.67)) + 'mm')
+                            # Wpisanie pozycji poczatku uszkodzenia
+                            sheetRow.append(str(float(x_scale_val + round(float((x - damageLen - greenCounting) * mmPerPix) * CONVERTER))/1000))
+                            # Wpisanie koloru uszkodzenia
+                            sheetRow.append('niebieski')
+                            # Wpisanie dlugosci uszkodzenia
+                            sheetRow.append(str(round(float((damageLen + greenCounting) * mmPerPix) * CONVERTER)))
                             damageLen = 0
                             greenCounting = 0
             pixLine += chanY
@@ -206,7 +254,7 @@ class ImageProcessing:
             if not showedWarning2:
                 wb.save('pomiaryUszkodzen.xlsx')
         except Exception:
-            WarningWindow('Proszę zamknąć Excela przed rozpoczęciem pomiarów! Pomiar nie zapisany!')
+            WarningWindow('Prosze zamknac Excela przed rozpoczeciem pomiarow! Pomiar niezapisany!')
         return
 
     @staticmethod
